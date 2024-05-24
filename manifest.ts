32b07a7fbeb838f96a42f811c1b279afd579d03b
Ion <teloslinux@protonmail.com>
2024-05-24 07:39:40 +0300

    Update manifest.ts

--- manifest.ts
+++ manifest.ts
@@ -77,7 +77,7 @@ export const manifest: Partial<ManifestOptions> = {
       ],
     },
     {
-      name: 'Create room'
+      name: 'Create room',
       url: './disclaimer',
       icons: [
         {
