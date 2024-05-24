cd0cd20a0ffb41313c35a64c01d51a360c205a8e
Ion <teloslinux@protonmail.com>
2024-05-24 20:14:43 +0300

    Update Disclaimer.tsx

--- src/pages/Disclaimer/Disclaimer.tsx
+++ src/pages/Disclaimer/Disclaimer.tsx
@@ -1,20 +1,24 @@
-import { useContext, useEffect } from 'react'
-import { ShellContext } from 'contexts/ShellContext'
+import { useContext, useEffect, useState } from 'react';
+import { ShellContext } from 'contexts/ShellContext';
 
 export const Disclaimer = () => {
-  const { setTitle } = useContext(ShellContext)
-  let[htmlFileString, setHtmlFileString] = useState();
+  const { setTitle } = useContext(ShellContext);
+  const [htmlFileString, setHtmlFileString] = useState('');
 
   async function fetchHtml() {
-    setHtmlFileString(await (await fetch(`direct.html`)).text());
+    const response = await fetch('direct.html');
+    const text = await response.text();
+    setHtmlFileString(text);
   }
+
   useEffect(() => {
     fetchHtml();
-  }, []);  
-  
-  return(
+  }, []);
+
+  return (
     <div className="Direct">
       <div dangerouslySetInnerHTML={{ __html: htmlFileString }}></div>
     </div>
   );
-}
+};
+
